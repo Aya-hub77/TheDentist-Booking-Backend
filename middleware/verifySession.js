ebfe35802c7e1a1580fc8d4e02fd5eaa5b2cc205
unknown <ayazerguine.dev@gmail.com>
2026-01-22 22:07:07 +0100

    modified verifySession

--- middleware/verifySession.js
+++ middleware/verifySession.js
@@ -1,7 +1,10 @@
 export const verifySession = (req, res, next) => {
+    console.log("Session object on request:", req.session);
   if (req.session && req.session.user) {
+    console.log("User found:", req.session.user);
     next();
   } else {
+    console.log("No user in session");
     res.status(401).json({ message: "Unauthorized" });
   }
 };
