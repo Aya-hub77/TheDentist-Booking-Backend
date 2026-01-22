9645d72619bf45e10c81505e1aca4479c222cac9
unknown <ayazerguine.dev@gmail.com>
2026-01-22 21:35:55 +0100

    change lax to none

--- middleware/session.js
+++ middleware/session.js
@@ -11,7 +11,7 @@ const sessionMiddleware = session({
     maxAge: 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: 'lax',
+    sameSite: 'none',
   },
 });
 
