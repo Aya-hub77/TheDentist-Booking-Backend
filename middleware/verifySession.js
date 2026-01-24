1bba9cdc923e142dea858978d45631cc2f0df1f9
unknown <ayazerguine.dev@gmail.com>
2026-01-24 17:05:13 +0100

    logs to verifySession

--- middleware/verifySession.js
+++ middleware/verifySession.js
@@ -1,4 +1,7 @@
 export const verifySession = (req, res, next) => {
+  console.log('Cookies:', req.cookies);
+  console.log('SessionID:', req.sessionID);
+  console.log('Session object:', req.session);
   if (req.session && req.session.user) {
     next();
   } else {
